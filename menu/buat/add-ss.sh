4f8cf66528d2bd42f9e5c437a6c03956d7de558a
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-09-29 01:36:17 +0700

    Update add-ss.sh

--- menu/buat/add-ss.sh
+++ menu/buat/add-ss.sh
@@ -14,14 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
-exit 0
-fi
-clear
+[ $MYIP = $MYIP ];
 source /var/lib/crot/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
