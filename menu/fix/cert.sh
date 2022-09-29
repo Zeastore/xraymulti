1da26a43c80a5c01282c9174a1483bc8a1154f2f
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-09-29 07:36:30 +0700

    Update cert.sh

--- menu/fix/cert.sh
+++ menu/fix/cert.sh
@@ -15,7 +15,7 @@ clear
 echo start
 sleep 0.5
 source /var/lib/crot/ipvps.conf
-domain=$(cat /etc/xray/domain)
+domain=$IP
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 cd /root/
 wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
@@ -23,7 +23,7 @@ bash acme.sh --install
 rm acme.sh
 cd .acme.sh
 echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
-bash acme.sh --register-account -m kimochilol@gmail.com
+bash acme.sh --register-account -m pribadi.no99@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
 cd
